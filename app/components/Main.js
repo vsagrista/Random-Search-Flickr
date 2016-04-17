93dc0caeb9e6d5df799a51a5efde7b84a281e607
Victor Sagrista Lopez <victorsagristalopez@gmail.com>
2016-04-17 17:15:45 +0200

    Installed underscore, pictures get displayed now

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom')
+var _ = require('underscore-node');
 
 var flickrUrl = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=89d5a9ac35a66a4e8fd31d57704df3ce&format=json&nojsoncallback=1&text=barcelona skyline&extras=url_o'
 
@@ -24,10 +25,8 @@ var CityPictures = React.createClass({
     if (!this.state.cities) return <div>Loading ...</div>;   
     else {
     	if (this.state.cities.photos !== undefined) 
-    		return (
-			    <div>
-			    {this.state.cities.photos.photo[0].url_o.toString()}
-			    </div>
+    		return (   
+			    <img src={_.sample(this.state.cities.photos.photo).url_o.toString()} alt="boohoo" className="img-responsive"/>    
 		  	)	
 		  	return <div>Loading ...</div>;	  
     }   
